7d9e80fe70c714611744a4fa3941b3fb61efeab1
nablam <lamribenn@gmail.com>
2021-02-10 13:08:36 -0500

    first commit done from vs. make sure adding a new arduino project check include proj in solution dir

---
+++ Sketch_BLEOLED/Sketch_BLEOLED.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch_BLEOLED.ino
+ Created:	2/10/2021 1:04:47 PM
+ Author:	lamri
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
